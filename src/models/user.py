5d6637f41e331debd90de5d57dda379517001bf8
Niall1985 <dcunhaniall@gmail.com>
2025-10-29 23:29:37 +0530

    edit

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.db import Base
 from argon2 import PasswordHasher, exceptions
 
@@ -11,6 +11,9 @@ class User(Base):
 
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
+
+    repositories = relationship("Repository", back_populates="user")
+
 
     def set_password(self, password: str) -> None:
         self.password = ph.hash(password)
