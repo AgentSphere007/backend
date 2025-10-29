4090048fef9804b2c3dc1714080712664f679580
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2025-10-29 23:00:33 +0530

    chore: Updating models

--- src/models/repository.py
+++ src/models/repository.py
@@ -1,16 +1,30 @@
-from sqlalchemy import Boolean, ForeignKey, String, Integer
+import enum
+from sqlalchemy import Boolean, Enum, ForeignKey, String, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.models import User
 from src.db import Base
 
 
+class RepositoryStatus(enum.Enum):
+    pending = "pending"
+    success = "success"
+    failed = "failed"
+
+
 class Repository(Base):
     __tablename__ = "repository"
+
     repo_url: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    repo_name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     model_name: Mapped[str] = mapped_column(String, nullable=False)
+
     rating: Mapped[int] = mapped_column(Integer, default=0)
     description: Mapped[str] = mapped_column(String)
     is_private: Mapped[bool] = mapped_column(Boolean)
+    status: Mapped[RepositoryStatus] = mapped_column(
+        Enum(RepositoryStatus), default=RepositoryStatus.pending
+    )
+    issues: Mapped[str | None] = mapped_column(String(500), nullable=True)
 
     user: Mapped["User"] = relationship(back_populates="repositories")
